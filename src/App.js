6967b5c6531afe9512bfa3d00269e4b9dd9eb57c
Raúl <Raul@MacBook-Air-de-Raul-2.local>
2021-08-02 23:48:04 +0200

    testing changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>RICK & MORTY FANPAGE</h1>
+      <h3>flipping cards</h3>
+    </div>
+  );
+}
+
+export default App;
