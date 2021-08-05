40e58d0dae22d107bdef2b5634bf2676d59f320f
Raúl <Raul@MacBook-Air-de-Raul-2.local>
2021-08-05 18:48:55 +0200

    changed title

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
      
-      <h1>RICK & MORTY FANPAGE</h1>
+      <h1>RICK & MORTY CHARACTERS</h1>
       {/* <Grid container spacing={4} justify='center'>
           <Grid item xs={12} sm={6} md={4}> */}
               <List/>
